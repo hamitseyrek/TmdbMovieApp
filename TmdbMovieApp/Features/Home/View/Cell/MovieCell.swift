f280fb692d918d3b02aa8ead492028767fc7dc5c
hamitseyrek <hamitseyrek@gmail.com>
2022-04-23 18:17:50 +0300

    edit some issues for MovieCell

--- TmdbMovieApp/Features/Home/View/Cell/MovieCell.swift
+++ TmdbMovieApp/Features/Home/View/Cell/MovieCell.swift
@@ -47,31 +47,7 @@ extension MovieCell {
         }
         
         titleLabel.text = "\(movie.title) (\(movie.releaseDate.formatted(.iso8601.year())))"
-        owerviewLabel.text = movie.overview        
+        owerviewLabel.text = movie.overview
         dateLabel.text = MyHelper().formatter.string(from: movie.releaseDate)
-    }
-}
-
-extension UIImageView {
-    
-    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
-        
-        contentMode = mode
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-            else { return }
-            DispatchQueue.main.async() { [weak self] in
-                self?.image = image
-            }
-        }.resume()
-    }
-    func downloaded(from link: String, contentMode mode: ContentMode = .scaleToFill) {
-        guard let url = URL(string: link) else { return }
-        downloaded(from: url, contentMode: mode)
     }
 }
